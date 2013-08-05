4f5f162ae292699759221a1d80db586875586f93
Heyjin Kim <kim.heyjin@gmail.com>
2013-08-04 20:54:00 -0400

    use ArgumentParser

--- heyjin.py
+++ heyjin.py
@@ -1,8 +1,20 @@
+from argparse import ArgumentParser
+
+def get_params():
+  parser = ArgumentParser(description="Heyjin's first argument control")
+  parser.add_argument('name', help='The name passed to the function')
+  return parser.parse_args()
+
 def who_is_awesome(name):
-  if name == 'Heyjin':
+  if name.upper() == 'HEYJIN':
     print "%s is AWESOME!" % name
   else:
     print "%s is NOT awesome." % name
 
+def main():
+  params = get_params()
+  name = params.name
+  who_is_awesome(name)
+
 if __name__ == '__main__':
-  who_is_awesome('Heyjin')
+  main()
